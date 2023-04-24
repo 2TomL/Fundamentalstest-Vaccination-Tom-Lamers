04bbca7c91088bb9bd536fde460006d6c47c1029
2TomL <lamerstom.123@gmail.com>
2023-04-24 14:48:19 +0200

    Final met BendAngle ok nu

--- src/Opdracht_2/App/RobotApp.java
+++ src/Opdracht_2/App/RobotApp.java
@@ -10,11 +10,11 @@ public class RobotApp {
 
         RobotApp myRobotApp = new RobotApp();
         CrazyRobot crazyRobot1 = new CrazyRobot("CrazyRobot1 Franky");
-        BendingRobot bendingRobot1 = new BendingRobot("BendingRobot1", 100);
+        BendingRobot bendingRobot1 = new BendingRobot("BendingRobot1", 360);
         LiftingRobot liftingRobot1 = new LiftingRobot("Lifterke",2.60);
 
         liftingRobot1.boot();
-        bendingRobot1.bend(200);
+        bendingRobot1.bend(400);
         bendingRobot1.bend(30.0);
         liftingRobot1.lift(1.60);
         liftingRobot1.lift(4.0);
--- src/Opdracht_2/BendingRobot.java
+++ src/Opdracht_2/BendingRobot.java
@@ -9,19 +9,29 @@ public class BendingRobot extends Robot{
     }
 
     public void bend(double bendAngle) {
-            double angle = bendAngle % 360;
-            if (angle > maxBendAngle) {
-                System.out.println("Bending NOT possible: " + maxBendAngle);
-            } else {
-                System.out.println("Bent angle is: " + angle);
-            }
-
-//        if (bendAngle > maxBendAngle) {
-//            System.out.println("Bending NOT possible: " + maxBendAngle);
-//        } else {
-//            System.out.println("Bent angle is: " + bendAngle);
-//        }
+        double angle = bendAngle % 360;
+        if (angle < 0) {
+            angle += 360;
+        }
+        if (angle > maxBendAngle) {
+            System.out.println("Bending NOT possible: " + maxBendAngle);
+        } else {
+            System.out.println("Bent angle is: " + angle);
+        }
     }
+//            double angle = bendAngle % 360;
+//            if (angle > maxBendAngle) {
+//                System.out.println("Bending NOT possible: " + maxBendAngle);
+//            } else {
+//                System.out.println("Bent angle is: " + angle);
+//            }
+//
+////        if (bendAngle > maxBendAngle) {
+////            System.out.println("Bending NOT possible: " + maxBendAngle);
+////        } else {
+////            System.out.println("Bent angle is: " + bendAngle);
+////        }
+//    }
 
     @Override
     public String toString() {
